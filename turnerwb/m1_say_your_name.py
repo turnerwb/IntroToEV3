872c60930825a4248c98606adc053324d2c0ce91
turnerwb <turnerwb@rose-hulman.edu>
2017-12-07 11:30:20 -0500

    Done?

--- turnerwb/m1_say_your_name.py
+++ turnerwb/m1_say_your_name.py
@@ -5,7 +5,7 @@ This module is meant to be your first program that you've made for EV3.
   Use the samples to learn the API calls needed (not much code to write)
 
 Authors: Dave Fisher and Wesley Turner.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 import ev3dev.ev3 as ev3
 
 # ------------------------------------------------------------------
